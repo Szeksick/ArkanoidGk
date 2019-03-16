fba68ebd8955456134f1ecdfffb8d62780c3263a
Konrad Gugała <33671971+Szeksick@users.noreply.github.com>
2019-03-16 23:35:39 +0100

    first commit

---
+++ desktop/src/pl/kgdev/arkanoidgk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.kgdev.arkanoidgk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.kgdev.arkanoidgk.ArkanoidGK;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArkanoidGK(), config);
+	}
+}
